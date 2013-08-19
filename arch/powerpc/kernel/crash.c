f2a0831b7eefbdce87cb0df39b980e85ec8177fe
tuxuser <webmaster@libxenon.org>
2013-08-19 14:18:51 -0400

    v2.6.38-rc3-xenon

--- arch/powerpc/kernel/crash.c
+++ arch/powerpc/kernel/crash.c
@@ -261,7 +261,7 @@ void crash_kexec_secondary(struct pt_regs *regs)
  */
 int crash_shutdown_register(crash_shutdown_t handler)
 {
-	unsigned int i, rc;
+	unsigned int i, rc = 0;
 
 	spin_lock(&crash_handlers_lock);
 	for (i = 0 ; i < CRASH_HANDLER_MAX; i++)
